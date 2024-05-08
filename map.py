43d8bd1b903a51b2221247e43976d314899145b2
adams <adams@DESKTOP-F0C7ND1>
2024-05-08 18:20:10 +0100

    Error handling and comments

--- map.py
+++ map.py
@@ -1,45 +1,57 @@
-import os.path
-import yaml
-from point import point
+import yaml                 # Importing the yaml module for YAML file handling
+from point import point     # Importing the point class, presumably used for managing map points
 
 class map:
     def __init__(self):
-        self.map_name = ""
-        self.points = []
+        self.map_name = ""  # Initializing map_name attribute to an empty string
+        self.points = []    # Initializing points attribute to an empty list
 
     def __str__(self):
-        return str(self.map_name) + str(self.points)
+        return str(self.map_name) + str(self.points)    # Returning a string representation of map_name and points
     
     def new(self, name):
-        self.map_name = name
-        self.points.clear()
+        self.map_name = name    # Setting the map_name attribute to the provided name
+        self.points.clear()     # Clearing the list of points
 
     def read(self, file_path):
         try:
-            with open(file_path, 'r') as stream:
-                values = yaml.safe_load(stream)
+            with open(file_path, 'r') as stream:    # Opening the file specified by file_path in read mode
+                values = yaml.safe_load(stream)     # Loading YAML data from the file into values
                 yaml.nodes
-                self.map_name = values["name"]
-                #print(values)
+                
+                if values is None:
+                    return False        # Return False if YAML data is empty or None
+                
+                self.map_name = values["name"]      # Setting map_name to the "name" field from the YAML data
 
-                self.points.clear()
-                for value in values["nodes"]:
-                    node = point(value)
-                    self.points.append(node)
-                return True
-        except:
-            return False
-        
-    def write(self): 
-        nodedata = []
-        for point in self.points:
-            nodedata.append(point.yaml_point(self.map_name))
-        data = {
-            "name" : self.map_name,
-            "metric_map" : "",
-            "meta" : {"last_updated": "2021-04-21_08-23-39"},
-            "metric_map" : self.map_name,
+                self.points.clear()     # Clearing the list of points
+                
+                if "nodes" in values:               # Looping through the "nodes" field in the YAML data
+                    for value in values["nodes"]:   # Creating a new point object from the data
+                        node = point(value)         # Appending the point object to the list of points
+                        self.points.append(node)    # Returning True to indicate successful reading
+                
+                return True             # Returning True to indicate successful reading
             
-            "nodes" : nodedata
-            }
-        return data
+        except Exception as e:
+            print(f"An error occurred while reading the YAML file: {e}")
+            return False                # Returning False if an exception occurs during reading
+    
+    # Function to write map data to a YAML file   
+    def write(self): 
+        try:
+            nodedata = []
+            for point in self.points:       #Puts all the nodes in the points array into a dictionary in YAML format
+                nodedata.append(point.yaml_point(self.map_name))
+            data = {                        #Defines the main body of the YAML file for saving
+                "name" : self.map_name,
+                "metric_map" : "",
+                "meta" : {"last_updated": "2021-04-21_08-23-39"},
+                "metric_map" : self.map_name,
+                
+                "nodes" : nodedata
+                }
+            return data
+        except Exception as e:
+            print(f"An error occurred while fetching the YAML file: {e}")
+            return False
--- point.py
+++ point.py
@@ -1,53 +1,58 @@
 class point:
-    def __init__(self):
+    def __init__(self):         # Initialize instance variables with default values
         self.name = "node"
 
         self.coord = (0, 0, 0)
         self.orientation = (0, 0, 0, 0)
 
-        self.edges = []
+        self.edges = []         # Empty list to store edges
 
     def __init__(self, n):
-        self.name = n["node"]["name"]
+        self.name = n["node"]["name"]                           # Extract the name from the dictionary
 
-        xcoord = n["node"]["pose"]["position"]["x"]
+        xcoord = n["node"]["pose"]["position"]["x"]             # Extract coordinates from the dictionary
         ycoord = n["node"]["pose"]["position"]["y"]
         zcoord = n["node"]["pose"]["position"]["z"]
         self.coord = (xcoord, ycoord, zcoord)
 
-        worientation = n["node"]["pose"]["orientation"]["w"]
+        worientation = n["node"]["pose"]["orientation"]["w"]    # Extract orientation from the dictionary
         xorientation = n["node"]["pose"]["orientation"]["x"]
         yorientation = n["node"]["pose"]["orientation"]["y"]
         zorientation = n["node"]["pose"]["orientation"]["z"]
         self.orientation = (worientation, xorientation, yorientation, zorientation)
 
-        self.edges = []
+        self.edges = []                                         # Extract edges from the dictionary
         for edge in n["node"]["edges"]:
             self.edges.append(edge["node"])
   
     def __str__(self):
-        return str(self.name) + str(self.coord) + str(self.orientation) + str(self.edges)
+        return str(self.name) + str(self.coord) + str(self.orientation) + str(self.edges)           # Return string representation of the point object
     
     def yaml_point(self,map):
-        edges = []
-        for edge in self.edges:
-            edge_yaml = {
-                "action": "move_base",
-                "edge_id": self.name + "_" + edge,
-                "node": edge
-                }
-            edges.append(edge_yaml)
-        yaml_data = {  
-        "meta": {
-            "map": map,
-            "node": self.name},
-        "node": {
-            "name": self.name,
-            "pose": {
-                "position": {"x": self.coord[0], "y": self.coord[1], "z": self.coord[2]},
-                "orientation": {"w": self.orientation[0], "x": self.orientation[1], "y": self.orientation[2], "z": self.orientation[3]}
-            },
-            "edges": edges
-        }
-        }
-        return yaml_data
+        try:
+            edges = []
+            for edge in self.edges:             # Adds each edge back to a Dictionary
+                edge_yaml = {                   # Defines and builds the structure for the edges section of YAML file
+                    "action": "move_base",
+                    "edge_id": self.name + "_" + edge,
+                    "node": edge
+                    }
+                edges.append(edge_yaml)
+                
+            yaml_data = {                       # Defines and builds the structure for the nodes section of YAML file
+            "meta": {
+                "map": map,
+                "node": self.name},
+            "node": {
+                "name": self.name,
+                "pose": {
+                    "position": {"x": self.coord[0], "y": self.coord[1], "z": self.coord[2]},
+                    "orientation": {"w": self.orientation[0], "x": self.orientation[1], "y": self.orientation[2], "z": self.orientation[3]}
+                },
+                "edges": edges
+            }
+            }
+            return yaml_data
+        except Exception as e:
+            print(f"An error occurred while Fetching the YAML file: {e}")
+            return False
